8ceb989aa025da0321904d79c624b2b43bb8483e
Glitch (glitch-hello-website) <none>
2024-03-02 11:38:04 +0000

    ⚓️🌠 Checkpoint ./style.css:65462025/123 ./script.js:65462025/252 ./index.html:65462025/133

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const noo = document.querySelector('#Noo');
+function playSound(){
+  const audio = new Audio('https://cdn.glitch.global/5d86e2b5-4513-45c2-bf63-5f7454722378/jump-sound.mp3?v=1709354914400');
+  audio.play();
+}
+const noo = document.querySelector('img');
 noo.onclick = () => {
-  noo.style.top = '-50px';
+  playSound();
+  noo.style.top = '-30px';
   setTimeout(()=> {
     noo.style.top = '0px';
   },1000);
