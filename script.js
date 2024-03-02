bb530ebc37b5f9184c05fb8887c7923f0c0f50c7
Glitch (glitch-hello-website) <none>
2024-03-02 04:37:35 +0000

    🏯😼 Checkpoint ./script.js:65462025/224 ./style.css:65462025/135 ./index.html:65462025/202

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function playSound (){
-  const audio = new Audio('https://www.youtube.com/watch?v=OY_mJkgRxuM');
-  audio.play();
-}
-const noo = document.querySelector('#noo');
-noo.onclick = () => {
-  playSound();
-  noo.style.top = '-50px';
-}
+
